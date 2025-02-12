09f24a00b67fc08abe9ef3e8ad739a3c4bf59f60
Amene <layla_1380@yahoo.com>
2025-02-12 13:58:13 -0600

    Add id parameter to all_items in app.py

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Item from "./Item";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <div>
+      <h1>Welcome to Easy Order</h1>
+      <Router>
+        <Routes>
+          <Route path='/items' element={<Item/> } />
+        </Routes>
+
+      </Router>
+    </div>
+  )
 }
 
 export default App;
--- server/app.py
+++ server/app.py
@@ -24,6 +24,7 @@ class Items(Resource):
         
         all_items = [
             {
+                'id': item.id,
                 'name': item.name,
                 'price': item.price
             }
