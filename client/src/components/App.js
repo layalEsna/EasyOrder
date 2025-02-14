ed5f0056b58e33899adff927afe122bf4debd960
Amene <layla_1380@yahoo.com>
2025-02-13 21:47:26 -0600

    Add check_session fetch in App.js to persist user login state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,10 +3,28 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Item from "./Item";
 import Login from "./Login";
 import ItemDetail from "./ItemDetail";
+import Cart from "./Cart";
 
 // import Signup from "./Signup";
 
 function App() {
+
+  const [customer, setCustomer] = useState(null)
+  useEffect(() => {
+    fetch('/check_session')
+      .then(res => {
+        if (!res.ok) {
+        throw new Error('Failed to fetch custome_id.')
+        }
+        return res.json()
+    })
+      .then(data => {
+        if(data.customer){
+      setCustomer(data)}
+    })
+    .catch(e => console.error(e))
+  }, [])
+
   return (
     <div>
       <h1>Welcome to Easy Order</h1>
@@ -15,6 +33,7 @@ function App() {
           <Route path='/items' element={<Item/> } />
           <Route path='/items/:item_id' element={<ItemDetail/> } />
           <Route path='/login' element={<Login/> } />
+          <Route path='/cart' element={<Cart/> } />
           {/* <Route path='/signup' element={<Signup/> } /> */}
         </Routes>
 
---
+++ client/src/components/Cart.js
@@ -0,0 +1,55 @@
+
+
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+
+
+function Cart() {
+
+    const [items, setItems] = useState([])
+    const navigate = useNavigate()
+
+
+    useEffect(() => {
+        fetch('/cart')
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Failed to fetch items.')
+                }
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+                setItems(data)
+            })
+            .catch(e => console.error(e))
+
+    }, [])
+
+
+    return (
+        <div>
+            <h2>Your Cart</h2>
+
+            console.log(items)
+
+            {items.length > 0 ? (
+                
+                items.map((order, index) => (
+                    <div key={index}>
+
+                        <h3>{order.selected_item.name}</h3>
+                        <p>Quantity: {order.quantity}</p>
+                        <p>Total price: ${(order.selected_item.price * order.quantity).toFixed(2)}</p>
+
+                    </div>
+                ))
+            ) : ''
+            }
+            <button onClick={() => navigate('/checkout')}>Check out</button>
+        </div>
+    )
+}
+
+export default Cart
