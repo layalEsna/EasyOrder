3d1884310ba2d1929468d94a63dba00b8f116208
Amene <layla_1380@yahoo.com>
2025-02-14 14:45:33 -0600

    Calculate the correct total price in the cart by using the sum method with item quantity.

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -17,27 +17,52 @@ function Cart({ customer }) {
             .then(data => setItems(data))
             .catch(e => console.error('Failed to fetch cart items:', e))
     }, [])
+    const totalPrice = items.reduce((sum, order) => sum + order.selected_item.price * order.quantity, 0)
 
 
     return (
+
         <div>
             <h2>Your Cart {customer && customer.username ? customer.username : "Guest"}</h2>
 
             {items.length > 0 ? (
-
                 items.map((order, index) => (
                     <div key={index}>
-
                         <h3>{order.selected_item.name}</h3>
                         <p>Quantity: {order.quantity}</p>
                         {/* <p>Total price: ${(order.selected_item.price * order.quantity).toFixed(2)}</p> */}
-
                     </div>
                 ))
-            ) : ''
-            }
+            ) : (
+                <p>Your cart is empty.</p>
+            )}
+
+            {items.length > 0 && <h3>Total: ${totalPrice.toFixed(2)}</h3>}
+            
             <button onClick={() => navigate('/confirmation')}>Checkout</button>
         </div>
+
+
+
+        // <div>
+        //     <h2>Your Cart {customer && customer.username ? customer.username : "Guest"}</h2>
+
+        //     {items.length > 0 ? (
+
+        //         items.map((order, index) => (
+        //             <div key={index}>
+
+        //                 <h3>{order.selected_item.name}</h3>
+        //                 <p>Quantity: {order.quantity}</p>
+        //                 {/* <p>Total price: ${(order.selected_item.price * order.quantity).toFixed(2)}</p> */}
+
+        //             </div>
+        //         ))
+        //     ) : ''
+        //     }
+        //     {/* {items.length > 0 && <h4>Total: ${(order.selected_item.price * order.quantity).toFixed(2)}</h4>} */}
+        //     <button onClick={() => navigate('/confirmation')}>Checkout</button>
+        // </div>
     )
 }
 
